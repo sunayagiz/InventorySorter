344481161222f3a6bf2fd81f6c74a766e289b94b
Sunay Y. Mandaci <sunayyagizmandaci@gmail.com>
2026-06-12 22:15:32 +0200

    feat: initial scaffolding and project plan

---
+++ src/main/java/com/sunay/inventorysorter/InventorySorter.java
@@ -0,0 +1,15 @@
+package com.sunay.inventorysorter;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InventorySorter implements ModInitializer {
+    public static final String MOD_ID = "inventorysorter";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("InventorySorter initialized! Sorting your world, one chest at a time.");
+    }
+}
